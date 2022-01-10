d6a8c4179485d0c81a33d91f9ec856517baadfdc
david-dkp <david.dekeuwer@gmail.com>
2022-01-10 08:37:11 +0100

    feat: add updateAuthUser method to userApi

--- apis/userApi.js
+++ apis/userApi.js
@@ -10,14 +10,16 @@ const getUser = async (id) => {
     return await axios.get("/api/users/" + id)
 }
 
-const updateUser = async (formData) => {
-    return await axios.put("/api/users", formData, {
+const updateAuthUser = async (formData) => {
+    return await axios.put("/api/user", formData, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
     })
 }
 
-module.exports.getUser = getUser
-module.exports.updateUser = updateUser
-module.exports.getAuthUser = getAuthUser
+module.exports = {
+    getAuthUser,
+    getUser,
+    updateAuthUser
+}
